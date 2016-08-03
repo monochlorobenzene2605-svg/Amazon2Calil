21b5b48fda24744fbc8297a154369b4535b1a6e9
87 <crycli87+git@gmail.com>
2016-08-03 20:10:29 +0900

    init

---
+++ Amazon2Calil.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Amazon2Calil
+// @namespace    http://tampermonkey.net/
+// @version      0.1b
+// @author       crycli87
+// @match        https://www.amazon.co.jp/*
+// @require      http://code.jquery.com/jquery-2.1.4.js
+// @grant        GM_log
+// ==/UserScript==
+
+var isbnSelector = "div#detail_bullets_id li:contains('ISBN-10')";
+
+(function main() {
+  if(!isBookPage()){
+    return;
+  }
+  var isbn = getIsbn();
+  var link2Calil = "<a href='https://calil.jp/book/" + isbn + "' target='_blank'>カーリルで開く</a>";
+  $("div#rightCol").append(link2Calil);
+})();
+
+function isBookPage(){
+  return $(isbnSelector).length !== 0;
+}
+
+function getIsbn(){
+  return parseInt($(isbnSelector).text().slice(-10));
+}
